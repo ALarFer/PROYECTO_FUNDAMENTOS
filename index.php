4fbcf5db60fc962f4123970c81dea0cd3019e769
ALarFer <93343557+ALarFer@users.noreply.github.com>
2024-11-25 13:45:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-  session_start();
+session_start();
 
-  require 'database.php';
+require 'database.php';
 
-  if (isset($_SESSION['user_id'])) {
+if (isset($_SESSION['user_id'])) {
     $records = $conn->prepare('SELECT id, email, password FROM users WHERE id = :id');
     $records->bindParam(':id', $_SESSION['user_id']);
     $records->execute();
@@ -12,35 +12,61 @@
     $user = null;
 
     if (count($results) > 0) {
-      $user = $results;
+        $user = $results;
     }
-  }
+}
 ?>
 
 <!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome to your login</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="assets/css/style.css"> <!-- Importación del archivo de estilos -->
+    <style>
+        body {
+            background-image: url('login-plataforma.png'); /* Fondo de index3.php */
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            height: 100vh;
+            margin: 0;
+            display: grid;
+            place-items: center;
+        }
+        .cuadro {
+            background-color: rgba(255, 255, 255, 0.7);
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.3);
+            width: 500px;
+            height: 400px;
+            text-align: center;
+        }
+        .cuadro img {
+            max-width: 100%;
+            height: auto;
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+<body>
 
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Welcome to your login</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="stylesheet" href="assets/css/style.css">
-    </head>
-    <body>
+<?php require 'partials/header.php'; ?>
 
-      <?php require 'partials/header.php'; ?>
+<?php if(!empty($user)): ?>
+    <br> Bienvenido. <?= $user['email']; ?>
+    <br> Te has logeado correctamente
+    <a href="logout.php">Logout</a>
+<?php else: ?>
+    <div class="cuadro">
+        <img src="sistemas.png" alt="FISEI"/>
+        <h3></h3>
+        <a href="login.php">Login</a> or <a href="signup.php">SignUp</a>
+    </div>
+<?php endif; ?>
 
-      <?php if(!empty($user)): ?>
-    <br> Binvenido. <?= $user['email']; ?>
-    <br>Te has logeado correctamente
-    <a href="logout.php">
-      Logout
-    </a>
-  <?php else: ?>
-    <h1>Por favor Ingresa o Registrate</h1>
-
-    <a href="login.php">Login</a> or
-    <a href="signup.php">SignUp</a>
-  <?php endif; ?>
-    </body>
+</body>
 </html>
+
